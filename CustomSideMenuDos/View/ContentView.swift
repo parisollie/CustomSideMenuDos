0f39c59c80f0b093b9f2ed1d6fefd29839aedadd
parisollie <pjff18@gmail.com>
2025-03-16 13:35:22 -0600

    Custom side menu

---
+++ CustomSideMenuDos/View/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  CustomSideMenu
+//
+//  Created by Paul F on 15/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        MainView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
