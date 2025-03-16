d2d93776f017759dbfed79540ab31ced816c2e00
parisollie <pjff18@gmail.com>
2025-03-16 13:34:13 -0600

    Initial Commit

---
+++ CustomSideMenuDos/CustomSideMenuDosApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomSideMenuDosApp.swift
+//  CustomSideMenuDos
+//
+//  Created by Paul F on 16/03/25.
+//
+
+import SwiftUI
+
+@main
+struct CustomSideMenuDosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
